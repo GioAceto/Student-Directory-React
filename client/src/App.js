e6a1d4f64bcd5a8789fd197c6c5b9f8b4b24fa64
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-09-28 17:40:15 -0400

    set up folder structure and scss files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useContext } from 'react';
+
+
+function App() {
+
+
+  return (
+    <div className="app">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
