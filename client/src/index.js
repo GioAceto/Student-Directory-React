1085dde7cb5e9682f8f7c63e6b1bbb6f6b84c235
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-10-10 15:08:12 -0400

    add content and styling for StudentsContainer

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,105 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 
+const students = [
+  {
+    name: 'Emily',
+    age: '20',
+    address: 'Salem, MA',
+    gpa: '3.70',
+    major: 'Biochemistry',
+    image: 'emily.svg'
+  },
+  {
+    name: 'Fiona',
+    age: '21',
+    address: 'Providence, RI',
+    gpa: '3.82',
+    major: 'Psychology',
+    image: 'fiona.svg'
+  },
+  {
+    name: 'Travis',
+    age: '20',
+    address: 'Keene, NH',
+    gpa: '3.50',
+    major: 'Mechanical Engineering',
+    image: 'travis.svg'
+  },
+  {
+    name: 'Chloe',
+    age: '22',
+    address: 'Bridgeport, CT',
+    gpa: '3.28',
+    major: 'Political Science',
+    image: 'chloe.svg'
+  },
+  {
+    name: 'Maya',
+    age: '19',
+    address: 'Boston, MA',
+    gpa: '3.80',
+    major: 'Graphic Design',
+    image: 'maya.svg'
+  },
+  {
+    name: 'Michael',
+    age: '20',
+    address: 'Boston, MA',
+    gpa: '3.70',
+    major: 'Biology',
+    image: 'michael.svg'
+  },
+  {
+    name: 'Noah',
+    age: '21',
+    address: 'Portland, ME',
+    gpa: '3.78',
+    major: 'Civil Engineering',
+    image: 'noah.svg'
+  },
+  {
+    name: 'Sarah',
+    age: '20',
+    address: 'Beverly, MA',
+    gpa: '4.00',
+    major: 'Biology',
+    image: 'sarah.svg'
+  },
+  {
+    name: 'Scott',
+    age: '19',
+    address: 'Philadelphia, PA',
+    gpa: '3.40',
+    major: 'Accounting',
+    image: 'scott.svg'
+  },
+  {
+    name: 'Nadine',
+    age: '19',
+    address: 'Montreal, QC',
+    gpa: '4.00',
+    major: 'Biology',
+    image: 'nadine.svg'
+  },
+  {
+    name: 'Salim',
+    age: '19',
+    address: 'Boston, MA',
+    gpa: '3.75',
+    major: 'Physics',
+    image: 'salim.svg'
+  },
+  {
+    name: 'Caleb',
+    age: '20',
+    address: 'Anaheim, CA',
+    gpa: '3.20',
+    major: 'Accounting',
+    image: 'caleb.svg'
+  }
+]
+
 const App = () => {
   return (
     <div className="app-container">
@@ -15,6 +114,7 @@ const App = () => {
           <UpdateStudentForm />
           <DeleteStudentForm />
         </div>
+        <StudentsContainer />
       </div>
     </div>
   )
@@ -105,6 +205,37 @@ const DeleteStudentForm = () => {
     </form>
   )
 }
+
+const StudentsContainer = () => {
+  return (
+    <div className="students-container" >
+      {students.map((student) => {
+        return (
+          <Student student={student} />
+        )
+      })}
+    </div>
+  )
+}
+
+const Student = (props) => {
+  const { name, age, address, gpa, major, image } = props.student
+  return (
+    <div className="profile">
+      <h2>{name}</h2>
+      <img className="avatar" src={image} alt="student pic"></img>
+      <div className="data-list">
+        <h3>Age: {age}</h3>
+        <h3>Home Address: {address}</h3>
+        <h3>GPA: {gpa}</h3>
+        <h3>Major: {major}</h3>
+        <h3>Student ID:</h3>
+        <div className="separation"></div>
+      </div>
+    </div>
+  )
+}
+
 
 
 
